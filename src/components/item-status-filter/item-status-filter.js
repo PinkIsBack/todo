b9882ef38c872f2555a6c79080ca7f02756331d5
PinkIsBack <mark.bukovich@gmail.com>
2021-12-22 20:25:55 +0200

    labelClick

--- src/components/item-status-filter/item-status-filter.js
+++ src/components/item-status-filter/item-status-filter.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './item-status-filter.css';
 
-const ItemStatusFilter = () => {
-  return (
+export default class ItemStatusFilter extends Component {
+  render() {
+    return(
     <div className="btn-group">
       <button type="button"
               className="btn btn-info">All</button>
@@ -12,7 +13,6 @@ const ItemStatusFilter = () => {
       <button type="button"
               className="btn btn-outline-secondary">Done</button>
     </div>
-  );
+    );
+  };
 };
-
-export default ItemStatusFilter;
