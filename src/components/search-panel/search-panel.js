dfa45d27bea1626248fa1c37b5bd21a938fd0f9d
PinkIsBack <mark.bukovich@gmail.com>
2021-12-22 20:55:34 +0200

    delete todos

--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -3,15 +3,11 @@ import React from "react";
 import './search-panel.css'
 
 const SearchPanel = () => {
-    const searchText = 'Type here to search'
-    const searchStyle = {
-      fontSize: '20px'
-    }
-  
-    return <input 
-      className="search-input"
-      style={searchStyle}
-      placeholder={searchText} />;
+    return(
+      <input 
+        className="form-control search-input"
+        placeholder="type to search" />
+    ) 
 };
 
 export default SearchPanel;
