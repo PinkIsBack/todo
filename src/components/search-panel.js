5920774cdeba7997a457a9c23ff9146ccf7fb421
PinkIsBack <mark.bukovich@gmail.com>
2021-12-22 17:56:37 +0200

    bootstrap

--- src/components/search-panel.js
+++ src/components/search-panel.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import './search-panel.css'
+
 const SearchPanel = () => {
     const searchText = 'Type here to search'
     const searchStyle = {
@@ -7,6 +9,7 @@ const SearchPanel = () => {
     }
   
     return <input 
+      className="search-input"
       style={searchStyle}
       placeholder={searchText} />;
 };
